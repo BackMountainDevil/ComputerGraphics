e656f691d2bf21289f2a4d779bf707505c0c5b1c
kearney <191615342@qq.com>
2020-11-20 17:21:52 +0800

    为中点算法加入延时，加强对比

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ComputerGraphics
 Computer Graphics 计算机图形学 python实现
+# 二维图形生成
+## 直线生成算法
+- 数值微分法
+- [中点划线法](./mpline.py)
+- Bresenham划线法
+## 圆弧绘制算法
--- mpline.py
+++ mpline.py
@@ -9,26 +9,30 @@
 @License :   GPL 3.0
 @Desc    :   计算机图形学-DAA改进后的中点算法实现，基于pygame的鼠标事件，
              鼠标点下一个点后画点，再点下一个点后由中点算法划线，可划多条，右键清空画面重置
+             首先画出来的是由pygame划线函数画出来的红线，延时1s后用中点算法划白线，以此作为对比
+
              参考《计算机图形学基础 OpenGL版》 徐文鹏 清华大学出版社
              3.1.4 中点划线法
 '''
-
+import time
 import pygame
 import sys
 
-width = 920
-height = 640
+width = 920     # 画板宽度
+height = 640    # 画版高度
+isp = False
 x1 = y1 = x2 = y2 = -1
 ps = (x1, y1)
 pe = (x2, y2)
 
 
+# 交换两个变量的值
 def swap(a, b):
     return b, a
 
 
+# 中点划线法画白线
 def MidPointLine(sur, x1, y1, x2, y2):
-    pygame.draw.line(sur, (255, 0, 0), (x1, y1), (x2, y2))
     tag = 0
     dx = x2 - x1
     dy = y2 - y1
@@ -36,7 +40,7 @@ def MidPointLine(sur, x1, y1, x2, y2):
         x1, y1 = swap(x1, y1)
         x2, y2 = swap(x2, y2)
         tag = 1
-    if (x1 > x2):   # 确保x1 < x2
+    if (x1 > x2):  # 确保x1 < x2
         x1, x2 = swap(x1, x2)
         y1, y2 = swap(y1, y2)
     a = y1 - y2
@@ -57,7 +61,7 @@ def MidPointLine(sur, x1, y1, x2, y2):
                 pygame.draw.circle(sur, (255, 255, 255), (y, x), 1, 1)  # 互换
             else:
                 pygame.draw.circle(sur, (255, 255, 255), (x, y), 1, 1)
-    else:   # 斜率 <= 0
+    else:  # 斜率 <= 0
         x = x2
         y = y2
         while (x > x1):
@@ -96,11 +100,18 @@ while True:
             pygame.draw.circle(screen, (255, 255, 255), event.pos, 1, 1)
             print('(', x1, ',', y1, ')', '  (', x2, ',', y2, ')')
             if (x2 > 0):
-                MidPointLine(screen, x1, y1, x2, y2)
-                x1 = y1 = x2 = y2 = -1
+                pygame.draw.line(screen, (255, 0, 0), (x1, y1),
+                                 (x2, y2))  # 调用内置函数划线画红线
+                pygame.display.update()
+                isp = True
             if (pygame.mouse.get_pressed()[2]):  # 鼠标右键按下，重置
                 x1 = y1 = x2 = y2 = -1
                 screen.fill((0, 0, 0))
                 print("Reset done")
+    if (isp):
+        isp = False
+        time.sleep(1)  # 延时1s后在启动中点划线法画白线
+        MidPointLine(screen, x1, y1, x2, y2)
+        x1 = y1 = x2 = y2 = -1
     pygame.display.update()
 sys.exit()
